3f53b8372bfa6fbaf6fa6a3b5a7800d9c65a2436
berit_k <bik39b@gmail.com>
2023-05-16 12:35:41 -0700

    Added js to change background from drop down list

---
+++ js/script.js
@@ -0,0 +1,19 @@
+var dropDownList = document.querySelector("#moods");
+var body = document.querySelector("body");
+
+dropDownList.addEventListener("change", function(e){
+    var mood = e.target.value;
+    if (mood === "happy"){
+        body.classList.add("happy");
+        body.classList.remove("sad");
+        body.classList.remove("passionate");
+    } else if (mood === "sad"){
+        body.classList.add("sad");
+        body.classList.remove("happy");
+        body.classList.remove("passionate");
+    } else if (mood === "passionate"){
+        body.classList.add("passionate");
+        body.classList.remove("happy");
+        body.classList.remove("sad");
+    }
+});
